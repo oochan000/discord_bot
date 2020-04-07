42b0e8a9b1465a3ccd5ff91cda86a1f784a3d97c
oochan000 <62881684+oochan000@users.noreply.github.com>
2020-04-07 12:18:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,11 +8,11 @@ client = discord.Client()
 async def loop():
     prenow = datetime.utcnow() + timedelta(hours=9)
     now = prenow.strftime('%y/%m/%d-%H:%M')
-    if now == '20/04/07-12:13':
+    if now == '20/04/07-12:20':
         alert_channel = client.get_channel(694093239954833412)
         msg = f'DSS説明会まであと15分！'
         await alert_channel.send(msg)
 
 loop.start()
 
-client.run("Njk0MTU0MTcxODAwMDkyNzUy.XosxQg.o7eHCTfDslYdOzR_N93YSxXIW1E")
+client.run("Njk0MTU0MTcxODAwMDkyNzUy.Xovwaw.QjYQAX2g1xjsbr0-ZOj0xa0pPWM")
