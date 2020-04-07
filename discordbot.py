d4f7a51248d6a100ad94375fd7a464f24d17cac5
oochan000 <62881684+oochan000@users.noreply.github.com>
2020-04-07 13:51:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,12 +10,12 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @tasks.loop(seconds=10)
 async def loop():
     prenow = datetime.utcnow() + timedelta(hours=9)
-    now = prenow.strftime('%y/%m/%d-%H:%M')
-    if now == '13:49':
+    now = prenow.strftime('%H:%M')
+    if now == '13:55':
         voice = await client.get_channel(692958909476110409).connect()
         voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
         await voice.disconnect()
-    elif now == '13:59':
+    elif now == '14:00':
         voice = await client.get_channel(692958909476110409).connect()
         voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
         await voice.disconnect()
