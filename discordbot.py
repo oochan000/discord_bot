cac7c6e75d9b2c94fa2a5a94f433df36ccd72d05
oochan000 <62881684+oochan000@users.noreply.github.com>
2020-04-07 12:52:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,21 @@
 import discord
 from discord.ext import tasks
 from datetime import datetime, timedelta
+import os
 
 client = discord.Client()
+
+token = os.environ['DISCORD_BOT_TOKEN']
 
 @tasks.loop(seconds=60)
 async def loop():
     prenow = datetime.utcnow() + timedelta(hours=9)
     now = prenow.strftime('%y/%m/%d-%H:%M')
-    if now == '20/04/07-12:23':
+    if now == '20/04/07-12:53':
         alert_channel = client.get_channel(694093239954833412)
         msg = f'DSS説明会まであと15分！'
         await alert_channel.send(msg)
 
 loop.start()
 
-client.run("Njk0MTU0MTcxODAwMDkyNzUy.Xovwaw.QjYQAX2g1xjsbr0-ZOj0xa0pPWM")
+client.run(token)
