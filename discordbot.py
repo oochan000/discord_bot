acfd22b9ba7f4306db3e3e90beeaade15b5244ff
oochan000 <62881684+oochan000@users.noreply.github.com>
2020-04-08 14:56:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,9 +23,6 @@ async def loop():
         voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
         await asyncio.sleep(130)
         await voice.disconnect()
-    elif now2 == '20/04/08-14:55':
-        alert_channel = await client.get_channel(692958909476110408)
-        await alert_channel.send("DSSまであと5分！")
 
 loop.start()
 
