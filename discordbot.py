a1fffb41863ee3dc1c11c06d9db635b8837f0346
oochan000 <62881684+oochan000@users.noreply.github.com>
2020-04-09 14:00:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,10 @@
 import discord
-from discord.ext import tasks, commands
+from discord.ext import tasks
 from datetime import datetime, timedelta
 import asyncio
 import os
 
-bot = commands.Bot(command_prefix="$")
+client = discord.Client()
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -13,23 +13,16 @@ async def loop():
     prenow = datetime.utcnow() + timedelta(hours=9)
     now = prenow.strftime('%H:%M')
     if now == '22:58':
-        voice = await bot.get_channel(692958909476110409).connect()
+        voice = await client.get_channel(692958909476110409).connect()
         voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
         await asyncio.sleep(130)
         await voice.disconnect()
     elif now == '23:58':
-        voice = await bot.get_channel(692958909476110409).connect()
+        voice = await client.get_channel(692958909476110409).connect()
         voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
         await asyncio.sleep(130)
         await voice.disconnect()
 
-@bot.command(aliases=["play", "play_s", "play_S", "play_Shannon", "play_Shannons", "play_Shannons_Lullaby", "play_shannon", "play_shannons", "play_shannons_lullaby"])
-async def play(ctx):
-    voice = await bot.get_channel(692958909476110409).connect()
-    voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
-    await asyncio.sleep(130)
-    await voice.disconnect()
-
 loop.start()
 
-bot.run(token)
+client.run(token)
