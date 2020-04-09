69b71dc5eb39642f2ada662ca0f1c463f4033119
oochan000 <62881684+oochan000@users.noreply.github.com>
2020-04-09 14:28:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,46 @@
 import discord
-from discord.ext import tasks
-from datetime import datetime, timedelta
 import asyncio
 import os
-
-client = discord.Client()
+from datetime import datetime, timedelta
+from discord.ext import commands
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@tasks.loop(seconds=30)
-async def loop():
-    prenow = datetime.utcnow() + timedelta(hours=9)
-    now = prenow.strftime('%H:%M')
-    if now == '22:58':
-        voice = await client.get_channel(692958909476110409).connect()
-        voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
-        await asyncio.sleep(130)
-        await voice.disconnect()
-    elif now == '23:58':
-        voice = await client.get_channel(692958909476110409).connect()
-        voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
-        await asyncio.sleep(130)
-        await voice.disconnect()
+client = commands.Bot(command_prefix='$')
 
-loop.start()
+alarm_time = '23:33'#24hrs
+channel_id = '692958909476110409'
+
+@client.event
+async def time_check():
+    await client.wait_until_ready()
+    while not client.is_closed:
+    	prenow = datetime.utcnow() + timedelta(hours=9)
+    	now = prenow.strftime('%H:%M')
+
+        channel = client.get_channel(channel_id)
+
+    	if now == '22:58':
+        	voice = await client.get_channel(692958909476110409).connect()
+        	voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
+		await asyncio.sleep(130)
+		await voice.disconnect()
+    	elif now == '23:58':
+        	voice = await client.get_channel(692958909476110409).connect()
+        	voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
+		await asyncio.sleep(130)
+		await voice.disconnect()
+	else
+		await asyncio.sleep(1)
+
+@client.event
+async def on_message(massage)
+	if message.content.startwith("$play")
+		voice = await client.get_channel(692958909476110409).connect()
+        	voice.play(discord.FFmpegPCMAudio('Shannons_Lullaby.mp3'))
+		await asyncio.sleep(130)
+		await voice.disconnect()
+
+client.loop.create_task(time_check())
 
 client.run(token)
